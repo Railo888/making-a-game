0f8b99f61a44aa49cb9b1023493dcf2bd08cf561
Railo888 <0ev5dv283225f1k@ezweb.ne.jp>
2024-01-19 21:52:14 +0900

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const randomAnswer = Math.floor(Math.random() * 3);
+    console.log(getComputerChoice);
+}
+
+function playRound(playerSection, computerSection); {
+    playerSection = playerSection.toLowerCase();
+
+    if (playerSection === computerSection) {
+        return 'Tie';
+    } else if ((playerSection === 'Rock' && computerSection === 'Scissors')
+        || (playerSection === 'Scissors' && computerSection === 'Paper')
+        || (playerSection === 'Paper' && computerSection === 'Rock')) {
+        return 'You win'
+    } else {
+        return 'You lose'
+    }
+}
+
+function game() {
+
+    for (let i = 1; i < 5; i++) {
+
+    }
+}
