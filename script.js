fafda81f0f6d1a1bfe42aba735e14acb465d3417
Railo888 <0ev5dv283225f1k@ezweb.ne.jp>
2024-01-23 16:01:13 +0900

    third commit

--- script.js
+++ script.js
@@ -1,31 +1,67 @@
-'use strict';
+'use strict'
 
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
-    const randomAnswer = Math.floor(Math.random() * 3);
-    console.log(getComputerChoice);
+async function play() {
+    const playerChoice = await getPlayerChoice();
+    const cpuChoice = getCpuChoice();
+
+    const result = getResult(playerChoice, cpuChoice);
+    displayResult(result);
 }
 
-function playRound(playerSection, computerSection) {
-    playerSection = playerSection.toLowerCase();
+function getPlayerChoice() {
+    return new Promise((resolve) => {
+        readline.question(
+            'Choose your hand (Rock, Paper, or Scissors): ',
+            (hand) => {
+                readline.close();
+                resolve(hand.trim());
+            }
+        );
+    });
+}
 
-    if (playerSection === computerSection) {
+function getCpuChoice() {
+    // TODO: implement
+    let cpuChoice = ['Rock', 'Scissors', 'Paper'];
+    let randomHand = Math.floor(Math.random() * 3);
+    return cpuChoice[randomHand];
+}
+
+function getResult(playerChoice, cpuChoice) {
+    // TODO: implement
+    if (playerChoice === cpuChoice) {
         return 'Tie';
-    } else if ((playerSection === 'Rock' && computerSection === 'Scissors')
-        || (playerSection === 'Scissors' && computerSection === 'Paper')
-        || (playerSection === 'Paper' && computerSection === 'Rock')) {
+    } else if (
+        playerChoice === 'Rock' && cpuChoice === 'Scissors'
+        || playerChoice === 'Scissors' && cpuChoice === 'Paper'
+        || playerChoice === 'Paper' && cpuChoice === 'Rock') {
         return 'You win'
     } else {
         return 'You lose'
     }
 }
 
-function loopGame() {
-    const gameScore = playRound(playerSection);
+function displayResult(result) {
+    // TODO: implement
+    return result;
+}
 
-    for (let i = 0; i < 5; i++) {
-        const result = playRound();
-        return 'next round';
+// play();
+
+const loopPlay = function () {
+    for (let i = 0; i = 5; i++) {
+        play();
     }
 }
+
+loopPlay();
+
+// まず、playerがR,S,Pのどれかを入力する。
+// 次に、PCUが受け取って、ランダムにR,S,Pを出す。
+// それから、プレイヤーとCPUの手が同じなら'Tie'。プレイヤーが勝つと'YOU win'を出力。CPUが勝つと'you lose'を出力。
+// この流れを、5回繰り返して多く勝ったほうが勝ち。
